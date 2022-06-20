12198266d2868710d08f1194edfe0fe69a557c56
David Rysdam <david@rysdam.org>
2022-06-20 10:24:39 -0400

    intersectionality

--- env.py
+++ env.py
@@ -30,15 +30,33 @@ class Line():
         if ax == None:
             fig, ax = plt.subplots()
         ax.plot(x, y)
+
+    def intersection(self, l2):
+        # https://www.cuemath.com/geometry/intersection-of-two-lines/
+        # I bet linear algebra would actually be faster...
+        # ((b1c2 - b2c1)/(a1b2 - a2b1), (c1a2 - c2a1)/(a1b2 - a2b1))
+        a1, b1, c1 = self.params
+        a2, b2, c2 = l2.params
+        num1 = b1*c2 - b2*c1
+        num2 = c1*a2 - c2*a1
+        denom = a1*b2 - a2*b1
+
+        # lines are parallel
+        if denom == 0:
+            return None
+
+        return np.array([num1/float(denom), num2/float(denom)])
+        print('here')
+        
         
 if __name__ == '__main__':
     fig, ax = plt.subplots()
 
     # y = 1x + 1
-    l1 = Line(1, -1, 0)
+    l1 = Line(1, -1, 1)
     l1.graph(ax)
     # y = -1x + 1
-    l2 = Line(-1, -1, 0)
+    l2 = Line(-1, -1, 1)
     l2.graph(ax)
     # y = 0
     l3 = Line(0, 1, 0)
@@ -47,4 +65,17 @@ if __name__ == '__main__':
     l4 = Line(1, 0, 0)
     l4.graph(ax)
 
+    x, y = l1.intersection(l2)
+    plt.plot(x, y, 'o')
+    x, y = l2.intersection(l1)
+    plt.plot(x, y, 'o')
+    # x, y = l1.intersection(l1)
+    # plt.plot(x, y, 'o')
+    x, y = l1.intersection(l3)
+    plt.plot(x, y, 'o')
+    # x, y = l3.intersection(l4)
+    # plt.plot(x, y, 'o')
+    # x, y = l3.intersection(l3)
+    # plt.plot(x, y, 'o')
+    
     plt.show()
